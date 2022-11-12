999582c520bf5d2775094896af9d69a619fb0318
wzh <464407764@qq.com>
2022-11-12 17:18:22 +0800

    测试hello llvm程序和global_show程序

---
+++ build/CMakeFiles/learnLLVM.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/learnLLVM.dir/main.cpp.o"
+  "CMakeFiles/learnLLVM.dir/main.cpp.o.d"
+  "learnLLVM"
+  "learnLLVM.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/learnLLVM.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/CMakeFiles/learnLLVM.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for learnLLVM.
+# This may be replaced when dependencies are built.
